6a922c618448b2080752bf0a29905070e860a38d
Anja <agoldschmidt@sogyo.nl>
2020-03-11 13:09:39 +0100

    first project structure

---
+++ src/main/java/nl/sogyo/agoldschmidt_food_matcher/AgoldschmidtFoodMatcherApplication.java
@@ -0,0 +1,13 @@
+package nl.sogyo.agoldschmidt_food_matcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgoldschmidtFoodMatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgoldschmidtFoodMatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/sogyo/agoldschmidt_food_matcher/AgoldschmidtFoodMatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.sogyo.agoldschmidt_food_matcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgoldschmidtFoodMatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
